4b90366ab70b78250204ba2fa9ea259d43ae08f3
Seungmin <rnsdls1997@gmail.com>
2025-10-28 22:36:25 +0900

    refactor: remove generic type from Chunk

--- src/main/java/framework/core/data/Chunk.java
+++ src/main/java/framework/core/data/Chunk.java
@@ -6,16 +6,14 @@ import framework.core.data.payload.Payload;
 /**
  * TCP/IP 에서 사용되는 청크를 나타내는 클래스입니다.
  * <p>
- * {@code Header} 타입의 헤더와 {@code Payload} 타입의 페이로드를 포함합니다. 
- * {@code Header}는 타입 매개변수 {@code T}로 지정됩니다.
+ * {@code Header} 타입의 헤더와 {@code Payload} 타입의 페이로드를 포함합니다.
  * 
- * @param <T> 헤더의 타입을 나타내는 제네릭 타입 매개변수입니다. {@code Header} 클래스를 상속받아야 합니다.
  */
-public final class Chunk <T extends Header> {
-    private T header;
+public final class Chunk {
+    private Header header;
     private Payload payload;
 
-    public void setHeader(T header) {
+    public void setHeader(Header header) {
         this.header = header;
     }
 
@@ -23,7 +21,7 @@ public final class Chunk <T extends Header> {
         this.payload = payload;
     }
 
-    public T getHeader() {
+    public Header getHeader() {
         return header;
     }
 
