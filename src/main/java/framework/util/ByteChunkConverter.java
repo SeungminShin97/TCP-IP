68d4464ee80a02d37606da6a9e2b75da5ae8b20e
Seungmin <rnsdls1997@gmail.com>
2025-10-29 23:51:17 +0900

    feat: add ByteChunkConverter

---
+++ src/main/java/framework/util/ByteChunkConverter.java
@@ -0,0 +1,29 @@
+package framework.util;
+
+import framework.core.data.Chunk;
+
+/**
+ * {@code byte[]}과 {@link Chunk} 간의 변환을 담당하는 유틸리티 클래스입니다.
+ */
+public final class ByteChunkConverter {
+
+    /**
+     * {@link Chunk}}를 {@code byte[]}로 변환합니다.
+     * @param chunk 변환할 {@link Chunk}
+     * @return 변환된 {@code byte[]}
+     */
+    public static byte[] ToBytes(Chunk chunk) {
+        //TODO: 구현 필요
+        return null;
+    }
+
+    /**
+     * {@code byte[]}를 {@link Chunk}로 변환합니다.
+     * @param bytes 변환할 {@code byte[]}
+     * @return 변환된 {@link Chunk}
+     */
+    public static Chunk ToChunk(byte[] bytes) {
+        //TODO: 구현 필요
+        return null;
+    }
+}
